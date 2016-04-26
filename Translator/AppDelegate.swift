061f317da6efd3a60b5ffd21fc54e70cd08900d2
Artem Tverdokhlebov <artem.tverdokhlebov@gmail.com>
2016-04-27 01:39:30 +0300

    Initial Commit

---
+++ Translator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Translator
+//
+//  Created by Artem Tverdokhlebov on 4/27/16.
+//  Copyright © 2016 Artem Tverdokhlebov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Translator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Artem Tverdokhlebov on 4/27/16.
+//  Copyright © 2016 Artem Tverdokhlebov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
